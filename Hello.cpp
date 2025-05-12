151dfb3d18a4cb6772e4e156408d708e37f7f79c
MhThorq <thoriqtnkmk432@gmail.com>
2025-05-12 17:35:10 +0700

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	cout << "Hello World\n";
-	cout << "Nama\t : Wisnala Aryawiduta\n";
+	cout << "Nama\t : Wisnala Ayoajgwmah\n";
 	cout << "NIM\t : 22.11.5168\n";
 	cout << "Jurusan\t : Informatika\n";
 	cout << "Alamat\t : Gunungkidul\n";
